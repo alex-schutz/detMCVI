074ab7f59a16573c8fcabe7aa00d30817ad9e9b1
Yang You <bboylevi89@gmail.com>
2024-02-29 19:03:30 +0000

    update belief particle interface

---
+++ include/BeliefParticles.h
@@ -0,0 +1,33 @@
+/* This file has been written and/or modified by the following people:
+ *
+ * Yang You
+ *
+ */
+
+#ifndef _BELIEFPARTICLES_H_
+#define _BELIEFPARTICLES_H_
+
+#include <iostream>
+#include <vector>
+#include <map>
+#include <any> // from C++ 17
+
+using namespace std;
+
+class BeliefParticles
+{
+private:
+    vector<any> particles;           // a vector of Any state particles
+    int size_particles = -1;
+public:
+    BeliefParticles(){};
+    ~BeliefParticles(){};
+    BeliefParticles(vector<any> &particles);
+    any SampleOneState() const;
+    int GetParticleSize();
+    double operator[](int i);
+    bool operator==(BeliefParticles &o);
+    void BuildBeliefSparse();
+};
+
+#endif
