bdcdf0eae683b257ade3ab2cce9515153f6e5e49
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-03-12 12:00:36 +0000

    Add upper bound calculation

---
+++ include/Bound.h
@@ -0,0 +1,28 @@
+/* This file has been written and/or modified by the following people:
+ *
+ * Yang You
+ * Alex Schutz
+ *
+ */
+
+#ifndef _BOUND_H_
+#define _BOUND_H_
+
+#include <unordered_map>
+using namespace std;
+
+using Belief = unordered_map<int, double>;
+
+/** @brief Return an upper bound for the value of the belief.
+ *
+ * The upper bound value of the belief is the expected sum of each possible
+ * state's MDP value
+ *
+ * @param belief A map from state indices to probabilities.
+ * @param sim A POMDP simulator object
+ * @return double
+ */
+
+double UpperBoundEvaluation(const Belief& belief, const PomdpInterface* sim);
+
+#endif /* !_BOUND_H_ */
---
+++ src/Bound.cpp
@@ -0,0 +1,18 @@
+#include "../include/Bound.h"
+
+#include "../include/QLearning.h"
+
+double UpperBoundEvaluation(const Belief& belief, const PomdpInterface* sim) {
+  // We only use the underlying MDP dynamics of sim to compute the value
+  auto q_engine =
+      QLearning(sim, epsilon, learning_rate, pb_random_explore, nb_restarts);
+  // Estimate the value of each state in the belief
+  for (const auto& [state, prob] : belief) q_engine.EstimateValue(state);
+
+  // Calculate the upper bound
+  double V_upper_b = 0.0;
+  for (const auto& [state, prob] : belief)
+    V_upper_b += prob * get<0>(q_engine.MaxQ(state));
+
+  return V_upper_b;
+}
