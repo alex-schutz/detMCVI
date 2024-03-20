660af25b13402b4d3dd5885dcfd0ea04b22c78ee
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-03-20 11:09:07 +0000

    Update AlphaVector classes

--- include/AlphaVectorFSC.h
+++ include/AlphaVectorFSC.h
@@ -2,24 +2,52 @@
  *
  * Yang You
  * Alex Schutz
- * 
+ *
  */
 
-class AlphaVectorFSC
-{
-private:
-    /* data */
-public:
-    AlphaVectorFSC(/* args */);
-    ~AlphaVectorFSC();
+#ifndef _ALPHAVECTORFSC_H_
+#define _ALPHAVECTORFSC_H_
+
+#include "AlphaVectorNode.h"
+
+class AlphaVectorFSC {
+ private:
+  std::vector<std::unordered_map<std::pair<int64_t, int64_t>, int64_t>> _eta;
+  std::vector<AlphaVectorNode> _nodes;
+  std::vector<int64_t> _action_space;
+  std::vector<int64_t> _observation_space;
+
+ public:
+  AlphaVectorFSC(int64_t max_node_size,
+                 const std::vector<int64_t>& action_space,
+                 const std::vector<int64_t>& observation_space)
+      : _eta(max_node_size, {}),
+        _nodes(),
+        _action_space(action_space),
+        _observation_space(observation_space) {}
+
+  ~AlphaVectorFSC() = default;
+
+  AlphaVectorNode& GetNode(int64_t nI) { return _nodes[nI]; }
+  size_t NumNodes() const { return _nodes.size(); }
+  void AddNode(const std::vector<int64_t>& state_particles,
+               const std::vector<int64_t>& action_space,
+               const std::vector<int64_t>& observation_space,
+               uint64_t seed = std::random_device{}()) {
+    _nodes.emplace_back(AlphaVectorNode(state_particles, action_space,
+                                        observation_space, seed));
+  }
+
+  /// @brief Return the eta value assosciated with node nI, action a and
+  /// observation o. Returns -1 if it does not exist.
+  int64_t GetEtaValue(int64_t nI, int64_t a, int64_t o) const;
+
+  void UpdateEta(int64_t nI, int64_t a, int64_t o, int64_t nI_new) {
+    _eta[nI][{a, o}] = nI_new;
+  }
+
+  const std::vector<int64_t>& GetActionSpace() { return _action_space; }
+  const std::vector<int64_t>& GetObsSpace() { return _observation_space; }
 };
 
-AlphaVectorFSC::AlphaVectorFSC(/* args */)
-{
-}
-
-AlphaVectorFSC::~AlphaVectorFSC()
-{
-}
-
-
+#endif /* !_ALPHAVECTORFSC_H_ */
--- include/AlphaVectorNode.h
+++ include/AlphaVectorNode.h
@@ -1,16 +1,86 @@
-class AlphaVectorNode
-{
-private:
-    /* data */
-public:
-    AlphaVectorNode(/* args */);
-    ~AlphaVectorNode();
+/* This file has been written and/or modified by the following people:
+ *
+ * Yang You
+ * Alex Schutz
+ *
+ */
+
+#ifndef _ALPHAVECTORNODE_H_
+#define _ALPHAVECTORNODE_H_
+
+#include <random>
+#include <unordered_map>
+#include <vector>
+
+class AlphaVectorNode {
+  using ValueMap = std::unordered_map<
+      int64_t,
+      std::unordered_map<int64_t, std::unordered_map<int64_t, double>>>;
+
+ private:
+  std::vector<int64_t> _state_particles;
+  std::unordered_map<int64_t, double> _Q_action;
+  std::unordered_map<int64_t, double> _R_action;  // expected instant reward
+  ValueMap _V_a_o_n;
+  std::unordered_map<int64_t, double> _V_node_s;
+  std::unordered_map<int64_t, int64_t> _V_node_s_count;
+  double _V_node;
+  std::unordered_map<int64_t, bool> _best_action_update;
+  mutable std::mt19937_64 _rng;
+
+ public:
+  AlphaVectorNode(const std::vector<int64_t>& state_particles,
+                  const std::vector<int64_t>& action_space,
+                  const std::vector<int64_t>& observation_space,
+                  uint64_t seed = std::random_device{}());
+  AlphaVectorNode(const std::vector<int64_t>& action_space,
+                  const std::vector<int64_t>& observation_space,
+                  uint64_t seed = std::random_device{}());
+
+  ~AlphaVectorNode() = default;
+
+  int64_t GetBestAction() const;
+
+  const std::unordered_map<int64_t, double>& GetActionObservationValues(
+      int64_t a, int64_t o) const {
+    return _V_a_o_n.at(a).at(o);
+  }
+
+  void InitR(const std::vector<int64_t>& action_space) {
+    _R_action = InitDoubleKeys(action_space);
+  }
+  void InitQ(const std::vector<int64_t>& action_space) {
+    _Q_action = InitDoubleKeys(action_space);
+  }
+  void InitVals(const std::vector<int64_t>& action_space,
+                const std::vector<int64_t>& observation_space) {
+    _V_a_o_n = InitValueMap(action_space, observation_space);
+  }
+  double V_node() const { return _V_node; }
+
+  int64_t SampleParticle() const;
+
+  double GetR(int64_t action) const { return _R_action.at(action); }
+  double GetQ(int64_t action) const { return _Q_action.at(action); }
+  void AddR(int64_t action, double reward) { _R_action[action] += reward; }
+  void AddQ(int64_t action, double q) { _Q_action[action] += q; }
+  void NormaliseQ(int64_t action, int64_t N) { _Q_action[action] /= N; }
+  void UpdateValue(int64_t action, int64_t observation, int64_t nI,
+                   double val) {
+    _V_a_o_n[action][observation][nI] += val;
+  }
+
+  /// @brief  Recalculate _V_node by finding the best action
+  void UpdateBestValue();
+
+ private:
+  std::unordered_map<int64_t, double> AlphaVectorNode::InitDoubleKeys(
+      const std::vector<int64_t>& action_space) const;
+  std::unordered_map<int64_t, bool> AlphaVectorNode::InitBoolKeys(
+      const std::vector<int64_t>& action_space) const;
+  ValueMap AlphaVectorNode::InitValueMap(
+      const std::vector<int64_t>& action_space,
+      const std::vector<int64_t>& observation_space) const;
 };
 
-AlphaVectorNode::AlphaVectorNode(/* args */)
-{
-}
-
-AlphaVectorNode::~AlphaVectorNode()
-{
-}
+#endif /* !_ALPHAVECTORNODE_H_ */
