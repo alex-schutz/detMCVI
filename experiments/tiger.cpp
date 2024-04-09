f8dd61c37c9147116dbff39c60df94d7f354af81
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-04-09 12:20:50 +0100

    Update tiger example

--- experiments/tiger.cpp
+++ experiments/tiger.cpp
@@ -52,25 +52,42 @@ int main() {
   const double noise = 0.15;
   auto pomdp = TigerPOMDP(noise);
 
+  const int64_t nb_particles_b0 = 10000;
+  const int64_t max_node_size = 10000;
+
   // Sample the initial belief
   std::vector<int64_t> particles;
-  for (int i = 0; i < 500; ++i) particles.push_back(pomdp.SampleStartState());
+  for (int i = 0; i < nb_particles_b0; ++i)
+    particles.push_back(pomdp.SampleStartState());
   const auto init_belief = BeliefParticles(particles);
 
   // Set the Q-learning policy
-  const auto q_policy = QLearningPolicy(0.9, 0.01, 10, 10, 10, 20, 0.01);
+  const int64_t max_sim_depth = 40;
+  const double learning_rate = 0.9;
+  const int64_t nb_episode_size = 10;
+  const int64_t nb_max_episode = 10;
+  const int64_t nb_sim = 20;
+  const double decay_Q_learning = 0.01;
+  const double epsilon_Q_learning = 0.01;
+  const auto q_policy = QLearningPolicy(
+      learning_rate, decay_Q_learning, max_sim_depth, nb_max_episode,
+      nb_episode_size, nb_sim, epsilon_Q_learning);
 
   // Initialise the FSC
-  const auto init_fsc = AlphaVectorFSC(10000, {0, 1, 2}, {0, 1});
+  const auto init_fsc = AlphaVectorFSC(max_node_size, {0, 1, 2}, {0, 1});
 
   // Run MCVI
   auto planner = MCVIPlanner(&pomdp, init_fsc, init_belief, q_policy);
-  const auto fsc = planner.Plan(40, 1000, 0.1, 30);
+  const int64_t nb_sample = 1000;
+  const double converge_thresh = 0.1;
+  const int64_t max_iter = 30;
+  const auto fsc =
+      planner.Plan(max_sim_depth, nb_sample, converge_thresh, max_iter);
 
-  fsc.GenerateGraphviz(std::cout, pomdp.actions, pomdp.observations);
+  fsc.GenerateGraphviz(std::cerr, pomdp.actions, pomdp.observations);
 
   // Simulate the resultant FSC
-  planner.SimulationWithFSC(20);
+  //   planner.SimulationWithFSC(20);
 
   return 0;
 }
