79d1a8f8354a000358c3a87dbb9b3f4de5e46b53
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-05-08 11:53:03 +0100

    Functionalise plotting

--- experiments/plotting.py
+++ experiments/plotting.py
@@ -1,87 +1,65 @@
 import plotly.express as px
-import pandas as pd
-import math
 
 output = "show"
 # output="png"
 # output="html"
 
 files = [
-    "eval_results_random_2024-05-03_14-09_9936525/ctp_results_random_all.csv",
-    "eval_results_random_2024-05-04_08-33_1170464/ctp_results_random_all.csv",
-    "eval_results_random_2024-05-06_09-13_2996797/ctp_results_random_all.csv",
+    "eval_results_random_2024-05-08_10-16/ctp_results_random.csv",
 ]
 
-df_from_each_file = (pd.read_csv(f) for f in files)
-df = pd.concat(df_from_each_file, ignore_index=True)
-df["Stochastic edge count"] = (df["State space size"] / df["Nodes"]).apply(math.log2)
-df = df.rename(
-    columns={
-        "avg_reward_difference": "Difference in average reward from AO*",
-        "Nodes": "CTP problem nodes",
-    }
-)
 
-xlabel = "AO* policy nodes"
-ylabel = "MCVI policy nodes"
-xmax = df[xlabel].max()
-ymax = df[xlabel].max()
-range_xy = [0, max(xmax, ymax)]
-fig = px.scatter(
-    df,
-    x=xlabel,
-    y=ylabel,
-    range_x=range_xy,
-    range_y=range_xy,
-    color="Difference in average reward from AO*",
-    trendline="ols",
-    title="Policy size",
-    color_continuous_midpoint=0,
-    color_continuous_scale="Spectral",
-    hover_data=[
-        "CTP problem nodes",
-        "State space size",
-        "Initial belief size",
-        "Stochastic edge count",
-    ],
-)
-fig.update_xaxes(constrain="domain")
-fig.update_yaxes(scaleanchor="x")
-if output == "show":
-    fig.show()
-elif output == "png":
-    fig.write_image("policy_size.png", scale=2)
-elif output == "html":
-    fig.write_html("policy_size.html")
+def plot_comparison(xlabel, ylabel, title, figname, output="show"):
+    xmax = df[xlabel].max()
+    ymax = df[xlabel].max()
+    range_xy = [0, 1.05 * max(xmax, ymax)]
+    fig = px.scatter(
+        df,
+        x=xlabel,
+        y=ylabel,
+        range_x=range_xy,
+        range_y=range_xy,
+        color="Difference in average reward from AO*",
+        trendline="ols",
+        title=title,
+        color_continuous_midpoint=0,
+        color_continuous_scale="Spectral",
+        hover_data=[
+            "CTP problem nodes",
+            "State space size",
+            "Initial belief size",
+            "Stochastic edge count",
+        ],
+    )
+    fig.update_xaxes(constrain="domain")
+    fig.update_yaxes(scaleanchor="x")
+    if output == "show":
+        fig.show()
+    elif output == "png":
+        fig.write_image(f"{figname}.png", scale=2)
+    elif output == "html":
+        fig.write_html(f"{figname}.html")
 
-xlabel = "AO* runtime (s)"
-ylabel = "MCVI runtime (s)"
-xmax = df[xlabel].max()
-ymax = df[xlabel].max()
-range_xy = [0, max(xmax, ymax)]
-fig = px.scatter(
-    df,
-    x=xlabel,
-    y=ylabel,
-    range_x=range_xy,
-    range_y=range_xy,
-    color="Difference in average reward from AO*",
-    trendline="ols",
-    title="Computation time",
-    color_continuous_midpoint=0,
-    color_continuous_scale="Spectral",
-    hover_data=[
-        "CTP problem nodes",
-        "State space size",
-        "Initial belief size",
-        "Stochastic edge count",
-    ],
-)
-fig.update_xaxes(constrain="domain")
-fig.update_yaxes(scaleanchor="x")
-if output == "show":
-    fig.show()
-elif output == "png":
-    fig.write_image("runtime.png", scale=2)
-elif output == "html":
-    fig.write_html("runtime.html")
+
+if __name__ == "__main__":
+    import pandas as pd
+    import math
+
+    df_from_each_file = (pd.read_csv(f) for f in files)
+    df = pd.concat(df_from_each_file, ignore_index=True)
+    df["Stochastic edge count"] = (df["State space size"] / df["Nodes"]).apply(
+        math.log2
+    )
+    df = df.rename(
+        columns={
+            "avg_reward_difference": "Difference in average reward from AO*",
+            "Nodes": "CTP problem nodes",
+        }
+    )
+
+    plot_comparison(
+        "AO* policy nodes", "MCVI policy nodes", "Policy size", "policy_size", output
+    )
+    plot_comparison(
+        "AO* runtime (s)", "MCVI runtime (s)", "Computation time", "runtime", output
+    )
