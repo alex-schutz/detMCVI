92e001d3d688733f8d1761ed836b5ca7c8ca93e8
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-05-15 10:06:44 +0100

    Update plots

--- experiments/plotting.py
+++ experiments/plotting.py
@@ -5,21 +5,26 @@ output = "show"
 # output="html"
 
 files = [
-    "eval_results_random_2024-05-08_10-16/ctp_results_random.csv",
+    "eval_results_random_2024-05-14_18-16/ctp_results_random.csv",
 ]
 
 
 def plot_comparison(xlabel, ylabel, title, figname, output="show"):
     xmax = df[xlabel].max()
     ymax = df[ylabel].max()
-    range_xy = [0, 1.05 * max(xmax, ymax)]
+    xmin = df[xlabel].min()
+    ymin = df[ylabel].min()
+    range_xy = [
+        min(xmin, ymin) - 0.05 * abs(min(xmin, ymin)),
+        max(xmax, ymax) + 0.05 * abs(max(xmax, ymax)),
+    ]
     fig = px.scatter(
         df,
         x=xlabel,
         y=ylabel,
         range_x=range_xy,
         range_y=range_xy,
-        color="Difference in average reward from AO*",
+        color="percentage_complete_difference",
         trendline="ols",
         title=title,
         color_continuous_midpoint=0,
@@ -29,6 +34,12 @@ def plot_comparison(xlabel, ylabel, title, figname, output="show"):
             "State space size",
             "Initial belief size",
             "Stochastic edge count",
+            "MCVI runtime (s)",
+            "AO* runtime (s)",
+            "AO* completed problem Average reward",
+            "MCVI completed problem Average reward",
+            "MCVI completed problem Percentage",
+            "AO* completed problem Percentage",
         ],
     )
     fig.update_xaxes(constrain="domain")
@@ -44,9 +55,11 @@ def plot_comparison(xlabel, ylabel, title, figname, output="show"):
 if __name__ == "__main__":
     import pandas as pd
     import math
+    import numpy as np
 
     df_from_each_file = (pd.read_csv(f) for f in files)
     df = pd.concat(df_from_each_file, ignore_index=True)
+    df.replace([np.inf, -np.inf], np.nan, inplace=True)
     df["Stochastic edge count"] = (df["State space size"] / df["Nodes"]).apply(
         math.log2
     )
@@ -61,5 +74,9 @@ if __name__ == "__main__":
         "AO* policy nodes", "MCVI policy nodes", "Policy size", "policy_size", output
     )
     plot_comparison(
-        "AO* runtime (s)", "MCVI runtime (s)", "Computation time", "runtime", output
+        "AO* completed problem Average reward",
+        "MCVI completed problem Average reward",
+        "Average reward",
+        "reward",
+        output,
     )
