93ff10a4a848e81b077885a6103d22e41f1429e6
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-05-16 11:12:00 +0100

    Add alternate AO* implementation

--- include/AOStar.h
+++ include/AOStar.h
@@ -6,8 +6,6 @@
 
 #pragma once
 
-#include <deque>
-
 #include "BeliefTree.h"
 
 namespace MCVI {
@@ -74,8 +72,7 @@ void RunAOStar(std::shared_ptr<BeliefTreeNode> initial_belief, int64_t max_iter,
         node->GetOrAddChildren(a, heuristic, eval_depth, eval_epsilon, pomdp);
       backup_list.push_back(node);
 
-      if (AOStarTimeExpired(iter_start, max_computation_ms))
-        return AOStarFinalise(backup_list);
+      if (AOStarTimeExpired(iter_start, max_computation_ms)) return;
     }
 
     AOStarFinalise(backup_list);
@@ -83,6 +80,105 @@ void RunAOStar(std::shared_ptr<BeliefTreeNode> initial_belief, int64_t max_iter,
   }
   std::cout << "AO* planning complete, reached maximum iterations."
             << std::endl;
+}
+
+bool FringeInGraph(
+    const std::vector<std::pair<std::shared_ptr<BeliefTreeNode>,
+                                std::vector<std::shared_ptr<BeliefTreeNode>>>>&
+        fringe,
+    const std::vector<std::shared_ptr<BeliefTreeNode>>& graph) {
+  for (const auto& node_ptr : fringe)
+    if (std::find(graph.begin(), graph.end(), node_ptr.first) != graph.end())
+      return true;
+  return false;
+}
+
+std::pair<std::shared_ptr<BeliefTreeNode>,
+          std::vector<std::shared_ptr<BeliefTreeNode>>>
+ChooseNode(std::vector<std::pair<std::shared_ptr<BeliefTreeNode>,
+                                 std::vector<std::shared_ptr<BeliefTreeNode>>>>&
+               fringe,
+           const std::vector<std::shared_ptr<BeliefTreeNode>>& graph,
+           std::mt19937_64& rng) {
+  if (fringe.empty()) return {nullptr, {}};
+
+  // Create a vector containing nodes that exist in both fringe and graph
+  std::vector<std::pair<std::shared_ptr<BeliefTreeNode>,
+                        std::vector<std::shared_ptr<BeliefTreeNode>>>>
+      common_nodes;
+  std::copy_if(
+      fringe.begin(), fringe.end(), std::back_inserter(common_nodes),
+      [&graph](const std::pair<std::shared_ptr<BeliefTreeNode>,
+                               std::vector<std::shared_ptr<BeliefTreeNode>>>&
+                   node_ptr) {
+        return std::find(graph.begin(), graph.end(), node_ptr.first) !=
+               graph.end();
+      });
+  if (common_nodes.empty()) return {nullptr, {}};
+
+  // Randomly select a node
+  std::uniform_int_distribution<size_t> distribution(0,
+                                                     common_nodes.size() - 1);
+  auto it = std::next(common_nodes.begin(), distribution(rng));
+  return *it;
+}
+
+void RunAOStar2(std::shared_ptr<BeliefTreeNode> initial_belief,
+                int64_t max_iter, int64_t max_computation_ms,
+                const PathToTerminal& heuristic, int64_t eval_depth,
+                double eval_epsilon, std::mt19937_64& rng,
+                SimInterface* pomdp) {
+  std::vector<std::pair<std::shared_ptr<BeliefTreeNode>,
+                        std::vector<std::shared_ptr<BeliefTreeNode>>>>
+      fringe = {{initial_belief, {}}};
+  std::vector<std::shared_ptr<BeliefTreeNode>> graph = {initial_belief};
+
+  const auto iter_start = std::chrono::steady_clock::now();
+  int64_t iter = 0;
+  while (++iter <= max_iter && FringeInGraph(fringe, graph)) {
+    const auto [belief_node, history] = ChooseNode(fringe, graph, rng);
+
+    // remove node from fringe set
+    auto it = std::find(fringe.begin(), fringe.end(),
+                        std::make_pair(belief_node, history));
+    if (it != fringe.end())
+      fringe.erase(it);
+    else
+      throw std::logic_error("Cannot find fringe node to erase");
+
+    // expand node
+    auto new_history = history;
+    new_history.push_back(belief_node);
+    for (int64_t a = 0; a < pomdp->GetSizeOfA(); ++a) {
+      const auto actNode = belief_node->GetOrAddChildren(
+          a, heuristic, eval_depth, eval_epsilon, pomdp);
+      for (const auto& [obs, obsNode] : actNode.GetChildren())
+        fringe.push_back({obsNode.GetBelief(), new_history});
+    }
+
+    // back up the graph
+    AOStarFinalise(new_history);
+
+    // rebuild the the graph
+    graph = {initial_belief};
+    size_t i = 0;
+    while (i < graph.size()) {
+      auto node = graph.at(i);
+      if (node->GetBestActUBound() != -1) {
+        for (const auto& [obs, obsNode] :
+             node->GetChildren(node->GetBestActUBound()))
+          graph.push_back(obsNode.GetBelief());
+      }
+      ++i;
+    }
+
+    if (AOStarTimeExpired(iter_start, max_computation_ms)) return;
+  }
+  if (!FringeInGraph(fringe, graph))
+    std::cout << "AO* planning complete, spanned the graph." << std::endl;
+  else
+    std::cout << "AO* planning complete, reached maximum iterations."
+              << std::endl;
 }
 
 }  // namespace MCVI
