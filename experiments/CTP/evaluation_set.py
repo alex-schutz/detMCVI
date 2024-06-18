79cffb0ed1f427e30b56cd065b8f0e67810eafa4
Alex Schutz <74842472+alex-schutz@users.noreply.github.com>
2024-06-18 09:56:05 +0100

    Run evaluations simultaneously

--- experiments/CTP/evaluation_set.py
+++ experiments/CTP/evaluation_set.py
@@ -8,6 +8,9 @@ import subprocess
 import pandas as pd
 import time
 import pickle
+from multiprocessing.pool import ThreadPool
+from multiprocessing import cpu_count
+import sys
 
 TIMEOUT = 60 * 60 * 20
 SET_SIZE = 10
@@ -18,32 +21,40 @@ max_time = {
     10: 2 * 60 * 1000,
     15: 10 * 60 * 1000,
     20: 60 * 60 * 1000,
+    30: 20 * 60 * 60 * 1000,
+    40: 60 * 60 * 60 * 1000,
     50: 5 * 60 * 60 * 1000,
+    100: 5 * 60 * 60 * 1000,
 }
 eval_ms = {
     5: 1,
     10: 2,
     15: 1000,
     20: 10 * 1000,
-    50: 60 * 1000,
+    30: 30 * 1000,
+    40: 60 * 1000,
+    50: 2 * 60 * 1000,
+    100: 20 * 60 * 1000,
 }
 
 
-def run_ctp_instance(N, i, results_folder):
-    outfile = f"{results_folder}/CTPInstance_{N}_{i}.txt"
-
+def instantiate_ctp():
     # Build files
     cmd = "cd build && make"
     p = subprocess.run(
         cmd,
         stderr=subprocess.PIPE,
-        stdout=subprocess.PIPE,
+        stdout=sys.stdout,
         shell=True,
     )
     if p.returncode != 0:
         print(p.stdout)
         print(p.stderr)
         raise RuntimeError(f"Build failed with returncode {p.returncode}")
+
+
+def run_ctp_instance(N, i, results_folder):
+    outfile = f"{results_folder}/CTPInstance_{N}_{i}.txt"
 
     with open(outfile, "w") as f:
         # Run solver
@@ -69,7 +80,25 @@ def run_ctp_instance(N, i, results_folder):
     return outfile, 0
 
 
+def work(params):
+    problem_size, i, results_folder, seed = params
+    print(f"Starting problem {i}")
+    outfile, error = run_ctp_instance(problem_size, i, results_folder)
+    if error:
+        return
+
+    # Summarise results
+    instance_result = parse_file(outfile)
+    instance_result["Seed"] = seed
+    instance_result["Set number"] = i
+
+    # Save as we go
+    df = instance_result
+    df.to_csv(f"{results_folder}/ctp_results_{i}.csv", index=False)
+
+
 if __name__ == "__main__":
+
     for problem_size in max_time.keys():
         timestr = time.strftime("%Y-%m-%d_%H-%M")
         results_folder = f"eval_results_{problem_size}x{SET_SIZE}_{timestr}"
@@ -85,32 +114,25 @@ if __name__ == "__main__":
         with open(f"{results_folder}/problem_graphs.pickle", "wb") as f:
             pickle.dump(problem_graphs, f)
 
+        tp = ThreadPool(min(cpu_count() - 1, 5))
         for i, (G, origin, goal, seed) in enumerate(problem_graphs):
             with open(GRAPH_FILE, "w") as f:
                 graph_to_cpp(G, origin, goal, f)
+            instantiate_ctp()
+            params = problem_size, i, results_folder, seed
+            tp.apply_async(work, (params,))
+            time.sleep(1)
 
-            outfile, error = run_ctp_instance(problem_size, i, results_folder)
-            if error:
-                continue
+        tp.close()
+        tp.join()
 
-            # Summarise results
-            instance_result = parse_file(outfile)
-            instance_result["Seed"] = seed
-            instance_result["Set number"] = i
-            results.append(instance_result)
-
-            # Save as we go
-            df = instance_result
-            if not headers_written:
-                df.to_csv(f"{results_folder}/ctp_results.csv", index=False)
-                headers_written = True
-            else:
-                df.to_csv(
-                    f"{results_folder}/ctp_results.csv",
-                    mode="a",
-                    index=False,
-                    header=False,
-                )
+        results = []
+        for i in range(SET_SIZE):
+            try:
+                with open(f"{results_folder}/ctp_results_{i}.csv", "r") as f:
+                    results.append(pd.read_csv(f))
+            except:
+                pass
 
         df = pd.concat(results, ignore_index=True, sort=False)
         df.to_csv(f"{results_folder}/ctp_results_all.csv", index=False)
